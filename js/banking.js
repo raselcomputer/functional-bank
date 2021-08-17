4401e219d8d3a986c2e97117a9f1aed68834fc47
rasel computer <raselcomputer75@gmail.com>
2021-08-17 11:38:42 +0600

    new function added & complete the project

--- js/banking.js
+++ js/banking.js
@@ -1,44 +1,38 @@
+const depositBtn = document.getElementById('deposit-button');
+const withdrawBtn = document.getElementById('withdraw-button');
 
 // only for deposit 
-document.getElementById('deposit-button').addEventListener('click', function(){
-    changeBalance("deposit")
-});
+depositBtn.addEventListener('click', function() {
+    changeBalance('deposit')
+})
 
 // only for withdraw 
-document.getElementById('withdraw-button').addEventListener('click', function(){
-    changeBalance("withdraw")
-});
+withdrawBtn.addEventListener('click', function() {
+    changeBalance('withdraw')
+})
 
-
-// function 
-
+// declare function 
 function changeBalance(type) {
-    console.log(type)
-    const newMoney = document.getElementById(`${type}-input`);
-    const newMoneyText = newMoney.value;
-    const newMoneyNumber = parseFloat(newMoneyText);
-    newMoney.value = '';
-
-    const previousTotal = document.getElementById(`${type}-total`);
-    const previousTotalText = previousTotal.innerText;
-    const previousTotalNumber = parseFloat(previousTotalText);
+    const newMoney = document.getElementById(`${type}-input`)
+    const newMoneyText = newMoney.value 
+    const newMoneyNumber = parseFloat(newMoneyText)
+    newMoney.value = ''
     
-    previousTotal.innerText = previousTotalNumber + newMoneyNumber;
+    const previousTotal = document.getElementById(`${type}-total`)
+    const previousTotalText = previousTotal.innerText
+    const previousTotalNumber = parseFloat(previousTotalText);
+
+    previousTotal.innerText = previousTotalNumber + newMoneyNumber
 
     // set balance 
-    const balanceTotal = document.getElementById('balance-total');
-    const balanceText = balanceTotal.innerText;
-    const newBalanceTotal = parseFloat(balanceText);
+    const balanceTotal = document.getElementById('balance-total')
+    const balanceTotalText = balanceTotal.innerText
+    const balanceTotalNumber = parseFloat(balanceTotalText)
 
-    // show balance 
-    if(type == "withdraw"){
-        balanceTotal.innerText = newBalanceTotal - newMoneyNumber;
+    if(type == 'withdraw'){
+        balanceTotal.innerText = balanceTotalNumber - newMoneyNumber
     }
     else{
-        balanceTotal.innerText = newBalanceTotal + newMoneyNumber;
+        balanceTotal.innerText = balanceTotalNumber + newMoneyNumber
     }
 }
-
-
-
-
